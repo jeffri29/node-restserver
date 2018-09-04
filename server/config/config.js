c65609daa38f77ae519236d83e6c5ebb87968aa0
Jeffri <jeffriortiz29@gmail.com>
2018-09-03 19:20:04 -0500

    Restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,21 @@
 //  Puerto
 // ========================
 process.env.PORT = process.env.PORT || 3000;
+
+// ========================
+//  Entorno
+// ========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ========================
+//  Base de datos
+// ========================
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb://cafecito:aleman29@ds143932.mlab.com:43932/cafe'
+}
+
+process.env.URLDB = urlDB;
